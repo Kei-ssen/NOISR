b9709c94f322bf43328fee9f4ee0af1e2b7ec23b
Kei-ssen <lk95tae@gmail.com>
2024-12-28 16:02:01 +0100

    base code, some sections

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// HAMBURUGER MENU 
+const burger = document.getElementById('burger');
+const navLinks = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+})
+
