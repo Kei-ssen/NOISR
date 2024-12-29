eeee0c4ff86301ba1945be46900bbd8d2a227d41
xiangsoto <xiangsoto.145@gmail.com>
2024-12-29 22:00:58 +0100

    current hits & most listened artists

--- js/script.js
+++ js/script.js
@@ -6,3 +6,36 @@ burger.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 })
 
+// PLAY/PAUSA REPRODUCTOR MÚSICA
+$(document).ready(function() {
+    $('.play').on('click', function() {
+        var $li = $(this).closest('li');
+        var audio = $li.find('audio')[0];
+        var playButton = $li.find('.play');
+        var pauseButton = $li.find('.pause');
+
+        $('audio').each(function() {
+            if (!$(this).is(audio)) {
+                this.pause();
+                $(this).closest('li').find('.play').show();
+                $(this).closest('li').find('.pause').hide();
+            }
+        });
+
+        audio.play();
+        playButton.hide();
+        pauseButton.show();
+    });
+
+    $('.pause').on('click', function() {
+        var $li = $(this).closest('li');
+        var audio = $li.find('audio')[0];
+        var playButton = $li.find('.play');
+        var pauseButton = $li.find('.pause');
+
+        audio.pause();
+        pauseButton.hide();
+        playButton.show();
+    });
+});
+
