c8bebae046cf766ed7dfc7013b734fa433dfddf1
xiangsoto <xiangsoto.145@gmail.com>
2025-01-03 19:30:48 +0100

    artists

--- js/script.js
+++ js/script.js
@@ -53,14 +53,14 @@ $(document).ready(function() {
 
     // MODO CLARO/OSCURO
     $("#to-dark-mode").click(function() {
-        $("#section1 .cs-item, #section1 .cs-intro, .artistTop-grid, #albumes .cs-container").css("background-color", "rgba(0, 0, 0, 0.2)");
+        $("#section1 .item, #section1 .intro, .artistTop-grid, #albumes .container, #artists .container").css("background-color", "rgba(0, 0, 0, 0.2)");
 
         $("#to-light-mode").removeClass("hide-cont");
         $("#to-dark-mode").addClass("hide-cont");
     });
 
     $("#to-light-mode").click(function() {
-        $("#section1 .cs-item, #section1 .cs-intro, .artistTop-grid, #albumes .cs-container").css("background-color", "rgba(255, 255, 255, 0.4)");
+        $("#section1 .item, #section1 .intro, .artistTop-grid, #albumes .container, #artists .container").css("background-color", "rgba(255, 255, 255, 0.4)");
 
         $("#to-dark-mode").removeClass("hide-cont");
         $("#to-light-mode").addClass("hide-cont");
@@ -72,18 +72,15 @@ $(document).ready(function() {
     });
 
     // CONTADOR
-
     var fechaActual = new Date();
     var fechaActual = new Date();
     var yearActual = fechaActual.getFullYear();
     const nuevoYear = yearActual;
     
-    // Fechas específicas para cada contador
     const fecha1 = new Date("Feb 17, " + nuevoYear + " 00:00:00");
     const fecha2 = new Date("Feb 25, " + nuevoYear + " 00:00:00");
     const fecha3 = new Date("Feb 28, " + nuevoYear + " 00:00:00");
     
-    // Si la fecha actual ya pasó la fecha objetivo, asignamos el próximo año
     if (fechaActual > fecha1) {
         fecha1.setFullYear(fecha1.getFullYear() + 1);
     }
@@ -121,8 +118,79 @@ $(document).ready(function() {
     
     const getTrueNumber = x => (x < 10 ? "0" + x : x);
     
-    // Llamamos a la función para cada contador con su fecha específica
     startCountdown('new1', fecha1);
     startCountdown('new2', fecha2);
     startCountdown('new3', fecha3);
+
+    // PARTICLES.JS
+    function initializeParticles(containerId, color) {
+        particlesJS(containerId, {
+            "particles": {
+                "number": {
+                    "value": 80,
+                    "density": {
+                        "enable": true,
+                        "value_area": 800
+                    }
+                },
+                "color": {
+                    "value": color
+                },
+                "shape": {
+                    "type": "circle",
+                    "stroke": {
+                        "width": 0,
+                        "color": "#000000"
+                    }
+                },
+                "opacity": {
+                    "value": 0.5,
+                    "random": false,
+                    "anim": {
+                        "enable": false
+                    }
+                },
+                "size": {
+                    "value": 15,
+                    "random": true,
+                    "anim": {
+                        "enable": false
+                    }
+                },
+                "line_linked": {
+                    "enable": true,
+                    "distance": 150,
+                    "color": "#ffffff",
+                    "opacity": 0.6,
+                    "width": 1
+                },
+                "move": {
+                    "enable": true,
+                    "speed": 6,
+                    "direction": "none",
+                    "random": false,
+                    "out_mode": "out"
+                }
+            },
+            "interactivity": {
+                "events": {
+                    "onhover": {
+                        "enable": true,
+                        "mode": "grab"
+                    },
+                    "onclick": {
+                        "enable": true,
+                        "mode": "push"
+                    }
+                }
+            },
+            "retina_detect": true
+        });
+    }
+    
+    initializeParticles("particles-js-1", "#a42fe3");
+    initializeParticles("particles-js-2", "#ffe571");
+    initializeParticles("particles-js-3", "#a42fe3");
+      
 });
+
